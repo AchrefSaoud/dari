2367e8b4c5c755f8c34e6c430e29aeee9cfe3bee
Achraf Saoud <achraf.saoud@etudiant-fst.utm.tn>
2025-04-04 01:10:02 +0100

    fix conflict

--- src/main/java/utm/tn/dari/security/config/SecurityConfig.java
+++ src/main/java/utm/tn/dari/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import utm.tn.dari.security.jwt.JwtAuthenticationFilter;
 import utm.tn.dari.security.services.UserService;
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
    // private final UserService userService;
@@ -44,7 +46,6 @@ public class SecurityConfig {
                     "/webjars/**"
                 ).permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults());
