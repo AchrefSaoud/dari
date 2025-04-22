5de33c156cf09200728f5e530c397e67e0439ef8
Mohamed amine Tlili <foxweb585@gmail.com>
2025-04-22 22:54:08 +0200

    fix filtering

--- src/main/java/utm/tn/dari/DariApplication.java
+++ src/main/java/utm/tn/dari/DariApplication.java
@@ -1,15 +1,26 @@
 package utm.tn.dari;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import utm.tn.dari.config.DockerElasticsearchLauncher;
 
 @SpringBootApplication
 @EnableAsync
 public class DariApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(DariApplication.class, args);
+
+
+		try {
+			DockerElasticsearchLauncher.lauchDockerContainer();
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		 SpringApplication.run(DariApplication.class, args);
 	}
 
 }
---
+++ src/main/java/utm/tn/dari/config/DockerElasticsearchLauncher.java
@@ -0,0 +1,54 @@
+package utm.tn.dari.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+
+public class DockerElasticsearchLauncher {
+
+    public static void lauchDockerContainer() throws IOException, InterruptedException {
+        // Check if the container already exists
+        ProcessBuilder checkContainer = new ProcessBuilder(
+                "docker", "ps", "-a", "--filter", "name=elasticsearch", "--format", "{{.Names}}"
+        );
+        Process checkProcess = checkContainer.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(checkProcess.getInputStream()));
+        String line = reader.readLine();
+        checkProcess.waitFor();
+
+        if (line != null && line.equals("elasticsearch")) {
+            // If container exists, try to start it (in case it's stopped)
+            ProcessBuilder start = new ProcessBuilder("docker", "start", "elasticsearch").inheritIO();
+            start.start().waitFor();
+            System.out.println("Elasticsearch container already exists. Starting it...");
+            Thread.sleep(25000);
+
+        } else {
+            // Pull the Elasticsearch image
+            ProcessBuilder pull = new ProcessBuilder(
+                    "docker", "pull", "docker.elastic.co/elasticsearch/elasticsearch:8.11.1"
+            ).inheritIO();
+            pull.start().waitFor();
+
+            // Run Elasticsearch container
+            ProcessBuilder run = new ProcessBuilder(
+                    "docker", "run", "-d",
+                    "-p", "9200:9200",
+                    "--name", "elasticsearch",
+                    "-e", "discovery.type=single-node",
+                    "docker.elastic.co/elasticsearch/elasticsearch:8.11.1"
+            ).inheritIO();
+            run.start().waitFor();
+            System.out.println("Elasticsearch container launched successfully.");
+        }
+    }
+
+}
